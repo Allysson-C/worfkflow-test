50d009009c9499bc17716e944bff338a0bb6cce3
Allysson C. da Silva <allysson.cheguera@hotmail.com>
2020-07-17 13:46:14 -0300

    Configuration environments

---
+++ src/config/post-install.ts
@@ -0,0 +1,35 @@
+import { exec, ChildProcess } from 'child_process';
+
+let command: ChildProcess;
+
+switch (process.env.ENVIRONMENT) {
+  case 'develop':
+    command = exec('ng build --aot');
+    break;
+  case 'deploy':
+    command = exec('ng build --aot --configuration=deploy --output-path dist');
+    break;
+  case 'production':
+    command = exec('ng build --aot --configuration=production --output-path dist');
+    break;
+
+  default:
+    command = exec('ng build --aot --configuration=develop');
+    break;
+}
+
+if (command !== undefined) {
+    command.stdout.on('data', (data) => {
+        console.log(data);
+    });
+
+    command.stderr.on('data', (data) => {
+        console.error(data);
+    });
+
+    command.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+} else {
+    console.error('process.env.ENV: ' + process.env.ENV);
+}
