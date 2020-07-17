1915c6bd119758f8be9340ff061e97e902f4ecdd
Allysson C. da Silva <allysson.cheguera@hotmail.com>
2020-07-17 14:24:13 -0300

    Correção

--- server.js
+++ server.js
@@ -1,37 +1,16 @@
-const path = require('path');
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-var router = express.Router();
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept,'
-  );
-  next();
-});
+// Serve only the static files form the dist directory
+// Replace the '/dist/<to_your_project_name>'
+app.use(express.static(__dirname + '/dist/'));
 
-var distDir = __dirname + '/dist/';
-app.use(express.static(distDir));
-
-// serve angular front end files from root path
-router.use('/', express.static('app', { redirect: false }));
-
-// rewrite virtual urls to angular app to enable refreshing of internal pages
-router.get('*', function(req, res, next) {
-  res.sendFile(path.resolve('dist/index.html'));
-});
-// Send all requests to index.html
-app.get('/*', function(req, res) {
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
-const port = process.env.PORT || 5000;
-// default Heroku port
-
-app.listen(port, function() {
-  console.log('App running on port' + port);
-});
-
-module.exports = router;
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 5000);
