ffeb3d0df88cc6b49ab84d0dd3d9f79f67564a3c
Allysson C. da Silva <allysson.cheguera@hotmail.com>
2020-07-17 15:05:21 -0300

    Correção

--- server.js
+++ server.js
@@ -4,18 +4,18 @@ const path = require('path');
 
 const app = express();
 
-var distDir = __dirname + '/dist/';
+var distDir = __dirname + '/dist/workflow-test/';
 app.use(express.static(distDir));
 
 // serve angular front end files from root path
 router.use('/', express.static('app', { redirect: false }));
 // rewrite virtual urls to angular app to enable refreshing of internal pages
 router.get('*', function(req, res, next) {
-  res.sendFile(path.resolve('dist/index.html'));
+  res.sendFile(path.resolve('dist/workflow-test/index.html'));
 });
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join(__dirname + 'dist/workflow-test/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000);
