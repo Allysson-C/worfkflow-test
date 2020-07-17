73a612ca251e8e96c4308827a370b9edb06def4e
Allysson C. da Silva <allysson.cheguera@hotmail.com>
2020-07-17 15:31:37 -0300

    server

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
-//Install express server
-const express = require('express');
 const path = require('path');
-
+const express = require('express');
 const app = express();
+var router = express.Router();
+
 
 var distDir = __dirname + '/dist/';
 app.use(express.static(distDir));
 
 // serve angular front end files from root path
 router.use('/', express.static('app', { redirect: false }));
+
 // rewrite virtual urls to angular app to enable refreshing of internal pages
 router.get('*', function(req, res, next) {
   res.sendFile(path.resolve('dist/index.html'));
 });
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+
+const port = process.env.PORT || 5000;
+// default Heroku port
+
+app.listen(port, function() {
+  console.log('App running on port' + port);
+});
+
+module.exports = router;
